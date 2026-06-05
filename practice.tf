33f6aeb7efdf0f297706601423feff2d8540e1a9
chandnibajaj <chandnibajaj1992@gmail.com>
2026-06-05 23:56:18 +0530

    permission added

--- practice.tf
+++ practice.tf
@@ -18,6 +18,6 @@ resource "azurerm_resource_group" "Practice" {
 }
 
 resource "azurerm_resource_group" "Practice1" {
-  name     = "practice_actions_3"
+  name     = "practice_actions_2"
   location = "central india"
 }
